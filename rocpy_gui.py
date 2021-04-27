a3edeeab31f5d729e025063cd1d6e62627ed8271
gbene <gabri.benedetti@gmail.com>
2021-04-27 23:20:57 +0200

    Basic I/O interface

--- rocpy_gui.py
+++ rocpy_gui.py
@@ -32,37 +32,50 @@ def tool_mohr():
 	print("Morh Circle!")
 
 
-def main_var(mi,GSI,D,s_ci,MR):
-	
-	mb = mi*np.exp((GSI-100)/(28-(14*D)))
-	s = np.exp((GSI-100)/(9-(3*D)))
-	a = 0.5+((1/6)*(np.exp(-GSI/15)-np.exp(-20/3)))
-	Ei = MR*s_ci
-	return(mb,s,a,Ei)
+def main_var(dict_values):
+	s_ci,mi,GSI,D,E_i,MR = dict_values.values()
+	dict_ind['mb'] = mi*np.exp((GSI-100)/(28-(14*D)))
+	dict_ind['s'] = np.exp((GSI-100)/(9-(3*D)))
+	dict_ind['a'] = 0.5+((1/6)*(np.exp(-GSI/15)-np.exp(-20/3)))
+	dict_ind['E_i'] = MR*s_ci
+	#return dict_ind.values()
 	
 
 def hoek_crit():
-	s_t,s_u,s_cm,s_3max = r_h.hoek(s_ci,mb,s,a,Ei,case,H,gamma)
+	s_t,s_u,s_cm,s_3max = r_h.hoek(dict_values['s_ci'],dict_ind['mb'],dict_ind['s'],dict_ind['a'],dict_ind['E_i'],case,H,gamma)
+	E_rm = dict_ind['E_i']*(0.02+((1-(dict_values['D']/2))/(1+np.exp((60+(15*dict_values['D'])-dict_values['GSI'])/11))))
 	print(s_t,s_u,s_cm,E_rm,s_3max)
 
 def mohr_crit():
-	_,_,_,s_3max = r_h.hoek(s_ci,mb,s,a,Ei,case,H,gamma)
-	phi,c = r_m.mohr(s_ci,mb,s,a,s_3max)
+	_,_,_,s_3max = r_h.hoek(dict_values['s_ci'],dict_ind['mb'],dict_ind['s'],dict_ind['a'],dict_ind['E_i'],case,H,gamma)
+	phi,c = r_m.mohr(dict_values['s_ci'],dict_ind['mb'],dict_ind['s'],dict_ind['a'],s_3max)
 	print(np.rad2deg(phi), c)
 
-mi = 10
-GSI = 50
-D=0
-s_ci = 30
-MR = 400
+def register_value(key,value):
+	dict_values[key]=float(value.get())
+	main_var(dict_values)
+	#mb,s,a,Ei = main_var(dict_values)
+	E_i.configure(state='normal')
+	E_i.delete(0,'end')
+	E_i.insert(0,dict_ind['E_i'])
+	E_i.configure(state='disabled')
+	print(dict_ind)
+
+def calc_ei(key,value):
+	dict_values[key]=float(value.get())
+	mb,s,a,Ei = main_var(dict_ind)
+
+
+dict_values = {'s_ci': 30, 'mi': 10 , 'GSI': 50, 'D': 0, 'E_i': 12000,'MR': 400}
+dict_ind = dict()
 
 H = 50
 gamma = 0.026
 case = 'tunnel'
 
-mb,s,a,Ei = main_var(mi,GSI,D,s_ci,MR)
+main_var(dict_values)
 
-E_rm = Ei*(0.02+((1-(D/2))/(1+np.exp((60+(15*D)-GSI)/11))))
+
 
 
 if __name__ == "__main__":
@@ -98,10 +111,59 @@ if __name__ == "__main__":
 
 
 	# define a main frame to which a secondary frame (tool frame) is attached. On the tool frame all of the tools can be attached
+
 	main_frame = tk.Frame(master=window)
 	main_frame.pack(fill=tk.BOTH,expand=True)
-	tools_frame = tk.Frame(master=window)
-	tools_frame.pack(fill=tk.BOTH,expand=True)	
-    
+	tools_frame = tk.Frame(master=main_frame)
+	tools_frame.pack()
+	
+	tools_frame.rowconfigure(0,minsize=50,weight=1)
+	tools_frame.columnconfigure([0,1,2,3,4,5],minsize=50,weight=1)
+	
+	s_ci_label = tk.Label(tools_frame,text='s_ci')
+	s_ci = tk.Entry(master=tools_frame)
+	s_ci_label.grid(row=1,column=0)
+	s_ci.grid(row=1,column=1)
+	s_ci.bind('<KeyRelease>',lambda event: register_value('s_ci',s_ci))
+	s_ci.insert(0,dict_values['s_ci'])
+	
+	mi_label = tk.Label(tools_frame,text='mi')
+	mi = tk.Entry(master=tools_frame)
+	mi_label.grid(row=2,column=0)
+	mi.grid(row=2,column=1)
+	mi.bind('<KeyRelease>',lambda event: register_value('mi',mi))
+	mi.insert(0,dict_values['mi'])
+	
+	GSI_label = tk.Label(tools_frame,text='GSI')
+	GSI = tk.Entry(master=tools_frame)
+	GSI_label.grid(row=3,column=0)
+	GSI.grid(row=3,column=1)
+	GSI.bind('<KeyRelease>',lambda event: register_value('GSI',GSI))
+	GSI.insert(0,dict_values['GSI'])
+	
+	D_label = tk.Label(tools_frame,text='D')
+	D = tk.Entry(master=tools_frame)
+	D_label.grid(row=4,column=0)
+	D.grid(row=4,column=1)
+	D.bind('<KeyRelease>',lambda event: register_value('D',D))
+	D.insert(0,dict_values['D'])
+	
+	E_i_label = tk.Label(tools_frame,text='E_i')
+	E_i = tk.Entry(master=tools_frame)
+	E_i_label.grid(row=5,column=0)
+	E_i.grid(row=5,column=1)
+	E_i.bind('<KeyRelease>',lambda event: register_value('E_i',E_i))
+	E_i.insert(0,dict_values['E_i'])
+	E_i.config(state='disabled')
+	
+	MR_label = tk.Label(tools_frame,text='MR')
+	MR = tk.Entry(master=tools_frame)
+	MR_label.grid(row=6,column=0)
+	MR.grid(row=6,column=1)
+	MR.bind('<KeyRelease>',lambda event: register_value('MR',MR))
+	MR.insert(0,dict_values['MR'])
+	
+	
+	
 	window.protocol("WM_DELETE_WINDOW",file_exit) #when closed propt the exit warning
 	window.mainloop()
